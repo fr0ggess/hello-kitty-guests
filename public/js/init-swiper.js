5b00c4d421419cf56a525e2280032c924fc251bc
Лидия <lidiya.lemeshenko@gmail.com>
2026-04-29 00:10:47 +0700

    добавила swiper

---
+++ public/js/init-swiper.js
@@ -0,0 +1,40 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const swiper = new Swiper('.swiper', {
+    slidesPerView: 1,
+    loop: true, // Бесконечная прокрутка
+
+     autoplay: {
+        delay: 3000, // Пауза между слайдами (в мс)
+        disableOnInteraction: false, // Продолжать после ручного перелистывания
+    },
+
+    // Пагинация
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true, // Кружочки можно нажимать
+        renderBullet: function(index, className) {
+            // Цветочек вместо кружочка
+            return '<span class="' + className + '">🎀</span>';
+        },
+    },
+
+    // Навигация
+    // navigation: {
+    //     nextEl: '.swiper-button-next',
+    //     prevEl: '.swiper-button-prev',
+    // },
+
+    breakpoints: {
+    // Для ширины экрана >= 768px
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 20
+        },
+        // Для ширины экрана >= 1024px
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 20
+        }
+    }
+    });
+});
